24d4fdd4a099ff329661385bd6b0a2d33791c7cf
Victor Sanches de Almeida <vs826156@gmail.com>
2025-03-17 21:18:02 -0300

    First commit

---
+++ src/test/java/com/construcoes/gestao_obras/GestaoObrasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.construcoes.gestao_obras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoObrasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
